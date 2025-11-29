228df916e5768c6989664576928a74c8ae6efdc5
MUHSIN-M-P <18muhsinmp@gmail.com>
2025-11-29 08:58:11 +0530

    project init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(),tailwind()],
+});
